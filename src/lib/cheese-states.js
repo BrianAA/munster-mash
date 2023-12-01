c5438293c783ca027b1ef776717f2347ce1c304c
Brian Alfaro <brianalfaro@thoughttolife.com>
2023-12-01 16:46:16 -0500

    initial test

---
+++ src/lib/cheese-states.js
@@ -0,0 +1,5 @@
+export const cheeseStates = {
+    walk: { name: "walk", anim: "/animations/walk.gif" },
+    dance: { name: "dance", anim: "/animations/bounce-1.gif" },
+    yanked: { name: "yanked", anim: "/animations/yanked.gif" }
+}
